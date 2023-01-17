05e7f2064c29c5b78078b6de34d0f859c50734d5
Noyer Romain <romainnoyer1995@gmail.com>
2023-01-18 00:59:56 +0100

    Fin de la Page Confirmation

---
+++ front/js/confirmation.js
@@ -0,0 +1,18 @@
+//------- CONFIRMATION -------//
+
+//------ ORDER ------//
+
+//-- Je déclare la fonction de la page de commande --//
+
+let order = () => {
+  //-- Je cible l'élément 'orderId' de la page --//
+
+  const orderId = document.getElementById("orderId");
+
+  //-- Je génère du HTML dans l'élément 'orderId' avec le contenu de la clé 'orderId' du local storage --//
+
+  orderId.innerHTML = localStorage.getItem("orderId");
+};
+order();
+
+//------- Fin CONFIRMATION -------//
